ce078fd7aefbae68bb061887ee40699535aeb9c3
ludoviclefevre <contact@ludoviclefevre.fr>
2015-10-07 18:12:22 +0200

    Thumbnail generation fix

--- client/app/post/post.controller.js
+++ client/app/post/post.controller.js
@@ -10,44 +10,48 @@
         return $routeParams.id;
       }
 
-      function getPost(cb) {
-        $http.get('/api/posts').then(function(data) {
+      var parseResponse = function(data) {
 
-          console.log(data)
-          var post = _.find(data.data, {
-            _id: getPostId()
-          })
-          console.log(post);
+        console.log(data)
+        var post = _.find(data.data, {
+          _id: getPostId()
+        })
+        console.log(post);
 
-          if (post.medias) {
+        if (post.medias) {
 
-            _.forEach(post.medias, function(m) {
-              $http.post('/api/getTempUrlRead', {
-                _id: getPostId(),
-                file: m.fileId
-              }).then(function(data) {
-                console.log(data.data);
-                $scope.s3Medias.push({
-                  url: data.data.url,
-                  thumbUrl: data.data.url,
-                  fileId: m.fileId
-                });
+          _.forEach(post.medias, function(m) {
+            $http.post('/api/getTempUrlRead', {
+              _id: getPostId(),
+              file: m.fileId
+            }).then(function(data) {
+              console.log(data.data);
+              $scope.s3Medias.push({
+                url: data.data.url,
+                thumbUrl: data.data.thumbUrl,
+                fileId: m.fileId
               });
-            })
+            });
+          })
 
-          }
+        }
 
-          cb(post)
-        });
-      }
+        return post;
+      };
+
+      var getPost = function(cb) {
+        $http.get('/api/posts')
+          .then(parseResponse)
+          .then(cb);
+      };
 
       $scope.goToList = function() {
         $location.path('/posts/');
-      }
+      };
 
       $scope.editPost = function() {
         $location.path('/postEdit/' + getPostId())
-      }
+      };
 
       //constructeur
       getPost(function(post) {
--- client/app/posts/posts.controller.js
+++ client/app/posts/posts.controller.js
@@ -2,34 +2,31 @@
   'use strict';
 
   angular.module('myChildApp')
-    .controller('PostsCtrl', function ($scope,$http,$location) {
+    .controller('PostsCtrl', function ($scope, $http, $location) {
 
-      function displayPosts(){
+      function displayPosts() {
         console.log('displayPosts');
-        $http.get('/api/posts').then(function(data){
-          $scope.posts = data.data;  
+        $http.get('/api/posts').then(function (data) {
+          $scope.posts = data.data;
 
         });
-      };
-
-      $scope.goToPost = function(post){
-        console.log(post._id)
-        $location.path('/post/' + post._id);
       }
 
-    	$scope.addPost = function(){
-    		if(!$scope.newPost.title){
-    			return;
-    		}
-        $http.post('/api/posts',$scope.newPost).then(function(res){
+      $scope.goToPost = function (post) {
+        console.log(post._id)
+        $location.path('/post/' + post._id);
+      };
+
+      $scope.addPost = function () {
+        if (!$scope.newPost.title) {
+          return;
+        }
+        $http.post('/api/posts', $scope.newPost).then(function (res) {
           var id = res.data.insertedIds[0];
           $location.path('/postEdit/' + id);
         });
-    	}
+      };
 
-
-      //constructeur
       displayPosts();
-
     });
 })();
